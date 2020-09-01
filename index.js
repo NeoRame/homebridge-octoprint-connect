5451e63c62efbf6784190db5e3baad6bab609646
msteitz <70521359+msteitz@users.noreply.github.com>
2020-09-01 18:22:00 +0200

    moved from 'request' to 'got'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var rp = require("request-promise");
+var got = require("got");
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
@@ -18,32 +18,12 @@ function OctoprintAccessory(log, config, api) {
 
   this.currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.OFF;
   this.accessories = [];
-  this.service = new Service.Thermostat(this.name);
+  this.service = new Service.TemperatureSensor(this.name);
 
   //Required
   this.service
-    .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-    .on('get', this.getCurrentHeatingCoolingState.bind(this))
-    .on('set', this.setCurrentHeatingCoolingState.bind(this));
-
-	this.service
-		.getCharacteristic(Characteristic.TargetHeatingCoolingState)
-		.on('get', this.getTargetHeatingCoolingState.bind(this))
-		.on('set', this.setTargetHeatingCoolingState.bind(this));
-
-  this.service
     .getCharacteristic(Characteristic.CurrentTemperature)
     .on('get', this.getCurrentTemperature.bind(this));
-
-	this.service
-		.getCharacteristic(Characteristic.TargetTemperature)
-		.on('get', this.getTargetTemperature.bind(this))
-		.on('set', this.setTargetTemperature.bind(this));
-
-  this.service
-		.getCharacteristic(Characteristic.TemperatureDisplayUnits)
-		.on('get', this.getTemperatureDisplayUnits.bind(this))
-		.on('set', this.setTemperatureDisplayUnits.bind(this));
 
   //Optional
 	this.service
@@ -52,109 +32,32 @@ function OctoprintAccessory(log, config, api) {
 }
 
 OctoprintAccessory.prototype.identify = function(callback) {
-  this.log('Identify requested');
+  //this.log('Identify requested');
   callback(null);
 };
 
 //Required
-OctoprintAccessory.prototype.getCurrentHeatingCoolingState = function(callback) {
-//  this.log('Getting current heating cooling state... GET ' + this.server + '/api/printer');
-
-  var options = {
-    method: 'GET',
-    uri: this.server + '/api/printer',
-    headers: {
-      "X-Api-Key": this.apiKey
-    },
-    json: true
-  };
-
-  rp(options)
-    .then(function(printerState) {
-      //console.log('Retrieved printer state: ' + JSON.stringify(printerState));
-      var currentTemperature = printerState.temperature.tool0.actual;
-      var targetTemperature = printerState.temperature.tool0.target;
-
-      if (targetTemperature > currentTemperature) {
-        callback(null, Characteristic.CurrentHeatingCoolingState.HEAT);
-      } else {
-        callback(null, Characteristic.CurrentHeatingCoolingState.COOL);
-      }
-    })
-    .catch(function(error) {
-      callback(error);
-    });
-};
-
-OctoprintAccessory.prototype.setCurrentHeatingCoolingState = function(value, callback) {
-  callback(null);
-};
-
-OctoprintAccessory.prototype.getTargetHeatingCoolingState = function(callback) {
-  callback(null, Characteristic.TargetHeatingCoolingState.AUTO);
-};
-
-OctoprintAccessory.prototype.setTargetHeatingCoolingState = function(value, callback) {
-  callback(null);
-};
-
 OctoprintAccessory.prototype.getCurrentTemperature = function(callback) {
 //  this.log('Getting current temperature... GET ' + this.server + '/api/printer');
 
+  var uri = this.server + '/api/printer';
+
   var options = {
-    method: 'GET',
-    uri: this.server + '/api/printer',
     headers: {
       "X-Api-Key": this.apiKey
     },
-    json: true
   };
 
-  rp(options)
-    .then(function(printerState) {
-      //console.log('Retrieved current printer state: ' + JSON.stringify(printerState));
-      var currentTemperature = printerState.temperature.tool0.actual;
-      console.log(currentTemperature);
-      callback(null, currentTemperature);
-    })
-    .catch(function(error) {
-      callback(error);
-    });
-};
-
-OctoprintAccessory.prototype.getTargetTemperature = function(callback) {
-//  this.log('Getting target temperature... GET ' + this.server + '/api/printer');
-
-  var options = {
-    method: 'GET',
-    uri: this.server + '/api/printer',
-    headers: {
-      "X-Api-Key": this.apiKey
-    },
-    json: true
-  };
-
-  rp(options)
-    .then(function(printerState) {
-      //console.log('Retrieved target printer state: ' + JSON.stringify(printerState));
-      var targetTemperature = printerState.temperature.tool0.target;
-      callback(null, targetTemperature);
-    })
-    .catch(function(error) {
-      callback(error);
-    });
-};
-
-OctoprintAccessory.prototype.setTargetTemperature = function(value, callback) {
-  callback(null);
-};
-
-OctoprintAccessory.prototype.getTemperatureDisplayUnits = function(callback) {
-  callback(null, Characteristic.TemperatureDisplayUnits.CELSIUS);
-};
-
-OctoprintAccessory.prototype.setTemperatureDisplayUnits = function(value, callback) {
-  callback(null);
+    (async () => {
+      try {
+        const response = await got(uri,options);
+        const jsonResponse = JSON.parse(response.body);
+        var currentTemperature =  jsonResponse.temperature.tool0.actual;
+        callback(null, currentTemperature);
+      } catch (error) {
+        callback(error);
+      }
+    })();
 };
 
 //Optional
